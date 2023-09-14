4b19b1dad30af3d8d147970bd488ac4ac031656b
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2023-09-14 10:24:38 -0300

    actualizando a la fecha 14092023

---
+++ 4 controles basicos/4 controles basicos/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace _4_controles_basicos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNuevoMensaje = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(95, 63);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(68, 26);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(73, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNuevoMensaje
+            // 
+            this.txtNuevoMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNuevoMensaje.Location = new System.Drawing.Point(52, 12);
+            this.txtNuevoMensaje.Name = "txtNuevoMensaje";
+            this.txtNuevoMensaje.Size = new System.Drawing.Size(182, 26);
+            this.txtNuevoMensaje.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtNuevoMensaje);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNuevoMensaje;
+    }
+}
+
