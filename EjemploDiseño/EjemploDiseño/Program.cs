91dd4ffc95ac88a58d285f8dd25fb2de629529f7
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2023-09-14 12:05:59 -0300

    front de app de escritorio

--- EjemploDiseño/EjemploDiseño/Program.cs
+++ EjemploDiseño/EjemploDiseño/Program.cs
@@ -15,7 +15,7 @@ namespace EjemploDiseño
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MenuLateral());
         }
     }
 }
